d4e31b7c9ac5ed72a3ce2fb2744d8ad0b19b4e07
Michaël Goossens <michael_goossens@peopleware.be>
2021-12-01 08:38:47 +0100

    Improvements before start of AoC

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,48 @@
 fun main() {
+
+    val day = 1;
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val instructions = input.get(0).split(", ").toList()
+        var direction = intArrayOf(0, 1)
+        var position = intArrayOf(0, 0)
+
+        for(instruction in instructions) {
+            val rotation = instruction.get(0)
+            val steps = Character.getNumericValue(instruction.get(1))
+
+            if (rotation == 'L') {
+                val newDirection = intArrayOf(-direction[1], direction[0])
+                direction = newDirection
+            } else if (rotation == 'R') {
+                val newDirection = intArrayOf(direction[1], -direction[0])
+                direction = newDirection
+            }
+
+            position[0] = position[0] + (steps * direction[0])
+            position[1] = position[1] + (steps * direction[1])
+        }
+
+        return Math.abs(position[0]) + Math.abs(position[1])
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun treatPart(part: Int, answer: Int) {
+        print("Submit part $part result $answer? (y|n): ")
+        if (readLine() == "y") {
+            sendAnswer(day, part, answer)
+        }
+    }
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput(String.format("Day%02d_test", day))
+    check(part1(testInput) == 12)
+
+    val input = readInput(String.format("Day%02d", day))
+
+    treatPart(1, part1(input))
+    treatPart(2, part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,11 +1,59 @@
 import java.io.File
 import java.math.BigInteger
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
 import java.security.MessageDigest
+
+fun main() {
+    getInput(1)
+}
+
+fun prepareRequest(inputUrl: String): HttpRequest.Builder {
+    return HttpRequest.newBuilder()
+        .uri(URI.create(inputUrl))
+        .header("cookie", "session=$SESSION")
+}
+
+fun send(request: HttpRequest): HttpResponse<String> {
+    return HttpClient.newBuilder().build()
+        .send(request, HttpResponse.BodyHandlers.ofString())
+}
+
+fun getInput(day: Int) {
+    val inputUrl = "https://adventofcode.com/$YEAR/day/$day/input"
+
+    val request = prepareRequest(inputUrl)
+        .GET()
+        .build()
+
+    val response = send(request)
+    val path = Paths.get("src", String.format("Day%02d.txt", day))
+    Files.write(path, response.body().toByteArray(StandardCharsets.UTF_8), StandardOpenOption.CREATE, StandardOpenOption.WRITE)
+}
+
+fun sendAnswer(day: Int, level: Int, answer: Int) {
+    val answerUrl = "https://adventofcode.com/$YEAR/day/$day/answer"
+
+    val request = prepareRequest(answerUrl)
+        .POST(HttpRequest.BodyPublishers.ofString("level=$level,answer=$answer"))
+        .build();
+
+    val response = send(request);
+    println(response.body())
+}
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput(name: String): List<String> {
+    return File("src", "$name.txt").readLines()
+}
 
 /**
  * Converts string to md5 hash.
