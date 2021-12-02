7bddaf8bf48c193557b07097dae0ee5f4cef0330
Michaël Goossens <michael_goossens@peopleware.be>
2021-12-02 01:02:42 +0100

    Preparing for day 2 and making a template

---
+++ src/Day02.kt
@@ -0,0 +1,39 @@
+fun main() {
+
+    val day = 2;
+
+    fun solve(input: List<String>, window: Int): Int {
+        return 0
+    }
+
+    fun part1(input: List<String>): Int {
+        return solve(input, 1)
+    }
+
+    fun part2(input: List<String>): Int {
+        return solve(input, 3)
+    }
+
+    fun treatPart(part: Int, answer: Int) {
+        print("Submit part $part result $answer? (y|n): ")
+        if (readLine() == "y") {
+            sendAnswer(day, part, answer)
+        }
+    }
+
+    // test if implementation meets criteria from the description for part 1, like:
+    val testInput = readInput(String.format("Day%02d_test", day))
+    check(part1(testInput) == 0)
+
+    // test was ok retrieve the real data
+    val input = readInput(String.format("Day%02d", day))
+
+    // get the answer with the real data for part 1
+    treatPart(1, part1(input))
+
+    // test if implementation meets criteria from the description for part 2, like:
+    check(part2(testInput) == 0)
+
+    // get the answer with the real data for part 2
+    treatPart(2, part2(input))
+}
