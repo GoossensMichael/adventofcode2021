19da8a25aeb449f5f2517bf5bd3e0d3b7b62c33c
Renato Rezende Costa <renato.rezende.costa@gmail.com>
2021-12-02 10:14:55 +0100

    Hints and comments
    Looks great tho!

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,9 +1,15 @@
 fun main() {
 
     val day = 1;
+    val dayWithPadding: String = day.toString().padStart(2, '0')
 
     fun solve(input: List<String>, window: Int): Int {
-        val depths = input.stream().mapToInt(Integer::parseInt).toArray()
+        // To use the stream mapping solution from kotlin, you could use: 
+        // val depths = input.stream().mapToInt(Integer::parseInt).toArray()
+        val depths = input.asSequence().map { it.toInt()}.toList()
+        // You could just map it directly as well, given it's just a step
+        val depths1 = input.map { it.toInt() }
+        
         var count = 0
         for(i in 0..depths.size-(window + 1)) {
             if (depths[i] < depths[i+window]) {
@@ -30,11 +36,14 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description for part 1, like:
-    val testInput = readInput(String.format("Day%02d_test", day))
+    // You can build strings directly using interpolation, even when padding the number. Not the best, but maybe it could be written before?
+    // val testInput = readInput(String.format("Day%02d_test", day))
+    val testInput = readInput("Day$dayWithPadding_test")
+    
     check(part1(testInput) == 7)
 
     // test was ok retrieve the real data
-    val input = readInput(String.format("Day%02d", day))
+    val input = readInput(String.format("Day$dayWithPadding", day))
 
     // get the answer with the real data for part 1
     treatPart(1, part1(input))
