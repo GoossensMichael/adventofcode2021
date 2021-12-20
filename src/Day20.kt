1212cda0cbd325c1428736b3e1e32a49d1cd78b8
Michaël Goossens <michael_goossens@peopleware.be>
2021-12-20 14:01:21 +0100

    Fixed day 20 to take the state of the infinite plane into account.

--- src/Day20.kt
+++ src/Day20.kt
@@ -3,7 +3,6 @@ fun main() {
     object : AoC(day = 20) {
 
         override fun part1(input: List<String>): Number {
-            println("P1")
             return solve(input, 2)
         }
 
@@ -12,7 +11,6 @@ fun main() {
         }
 
         override fun part2(input: List<String>): Number {
-            println("P2")
             return solve(input, 50)
         }
 
@@ -22,31 +20,45 @@ fun main() {
 
         private fun List<String>.parse() = Pair(this[0], this.subList(2, this.size))
 
+
         private fun solve(input: List<String>, repeat: Int): Int {
             val (algorithm, inputImage) = input.parse()
 
+            var infinitePlaneState = false
             var result = inputImage
             repeat(repeat) {
-                result = enhance(algorithm, result)
-                result.forEach { println(it) }
-                println()
+                result = enhance(algorithm, result, infinitePlaneState)
+
+                // determine state of the infinite plane (light = true, dark = false)
+                infinitePlaneState = infinitePlaneSwitch(algorithm, infinitePlaneState)
             }
 
             return result.sumOf { line -> line.count { pixel -> pixel == '#' } }
         }
 
-        private fun enhance(algorithm: String, image: List<String>): List<String> {
+        private fun infinitePlaneSwitch(algorithm: String, currentInfinitePlaneState: Boolean): Boolean {
+            // The infinite plane on blinks when the first item in the algorithm is # and the last item is .
+            // The infinite plane stays dark if the first item in the algorithm is .
+            // The infinite plane turns light after the first round and stays that way if the first and last item in the algorithm is #
+            return when {
+                algorithm[0] == '.' -> false
+                algorithm[0] == '#' && algorithm[algorithm.length - 1] == '.' -> !currentInfinitePlaneState
+                algorithm[0] == '#' && algorithm[algorithm.length - 1] == '#' -> true
+                else -> error("All cases are covered, this error should never show (ae3ef3)")
+            }
+        }
+
+
+        private fun enhance(algorithm: String, image: List<String>, infinitePlaneState: Boolean): List<String> {
             fun isWithinBounds(x: Int, y: Int) =
                 x >= 0 && x < image[0].length && y >= 0 && y < image.size
 
             fun encode(i: Int, j: Int): Char {
                 var mask = ""
-                for (x in i-1..i+1) {
-                    for (y in j-1..j+1) {
-                        if (isWithinBounds(x, y)) {
-                            mask += if (image[x][y] == '#') 1 else 0
-                        } else {
-                            mask += 0
+                for (x in i - 1..i + 1) {
+                    for (y in j - 1..j + 1) {
+                        mask += if (isWithinBounds(x, y) && image[x][y] == '#') 1 else {
+                            if (!isWithinBounds(x, y) && infinitePlaneState) 1 else 0
                         }
                     }
                 }
